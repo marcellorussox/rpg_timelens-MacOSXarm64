3e9783ee74b5da64f5d871c52b13d664acc78e3b
Marcello <marcellorusso.x@gmail.com>
2025-01-15 19:30:01 +0100

    5 skip e non 3

--- run_timelens.sh
+++ run_timelens.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-skip=3
-insert=3
+skip=5
+insert=5
 base_path="test"
 
 # Itera su ogni dataset
@@ -22,7 +22,15 @@ for dataset in "$base_path"/*; do
         # Crea la cartella di output se non esiste
         mkdir -p "$output_path"
 
+        # Misura il tempo di esecuzione
+        start_time=$(date +%s)
+
         # Esegui il modello
         python -m timelens.run_timelens checkpoint.bin "$events_path" "$images_path" "$output_path" $skip $insert
+
+        end_time=$(date +%s)
+        elapsed_time=$((end_time - start_time))
+
+        echo "Finished processing $(basename "$dataset") in $elapsed_time seconds."
     fi
 done

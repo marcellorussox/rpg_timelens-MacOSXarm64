11ff03f361b12bb221b0a8a3a823b0c0fcd652b4
Marcello <marcellorusso.x@gmail.com>
2025-01-19 16:56:31 +0100

    centralizzazione in un quaderno jupyter

--- evaluation/run_evaluation.sh
+++
@@ -1,2 +0,0 @@
-# python evaluation/quantitative_evaluation.py evaluation/results_folder --num_processes 4 --output_file output.yaml --num_skips 1
-python evaluation/evaluation.py evaluation/results_folder --num_skips 1
